2bf3bff8e293432bb32a5f94c2d8cbaf29fcb360
Anton Chebanenko <106664982+antchb@users.noreply.github.com>
2022-07-10 14:56:45 +0200

    Add 'Welcome To Java' solution

---
+++ Java/Introduction/Welcome to Java!/Solution.java
@@ -0,0 +1,12 @@
+// HackerRank [https://www.hackerrank.com]
+
+// Link to the problem:
+// https://www.hackerrank.com/challenges/welcome-to-java
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
